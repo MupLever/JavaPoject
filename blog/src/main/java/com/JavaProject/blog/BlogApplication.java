056e0e7130af0f3c41c4536f52592dca7b8592c4
bmstujunior <8willox8@gmail.com>
2022-09-30 23:22:42 +0300

    create project

---
+++ blog/src/main/java/com/JavaProject/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.JavaProject.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
