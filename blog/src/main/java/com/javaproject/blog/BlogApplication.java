4d50815db1985ef6387648440e78351ae951b321
bmstujunior <8willox8@gmail.com>
2022-10-01 19:46:47 +0300

    first controller

--- blog/src/main/java/com/javaproject/blog/BlogApplication.java
+++ blog/src/main/java/com/javaproject/blog/BlogApplication.java
@@ -1,4 +1,4 @@
-package com.JavaProject.blog;
+package com.javaproject.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- blog/src/test/java/com/javaproject/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/javaproject/blog/BlogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.JavaProject.blog;
+package com.javaproject.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
