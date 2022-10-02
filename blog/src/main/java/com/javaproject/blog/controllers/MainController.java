a3f065997d3a24f883536aface1760c131230b98
bmstujunior <8willox8@gmail.com>
2022-10-02 18:26:19 +0300

    create folder with partials

--- blog/src/main/java/com/javaproject/blog/controllers/MainController.java
+++ blog/src/main/java/com/javaproject/blog/controllers/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 	
 	@GetMapping("/")
-	public String greeting(Model model) {
+	public String home(Model model) {
 		model.addAttribute("title", "Главная страница");
 		return "home";
 	}
